0b5d6e3747d03432ca28bdca00715212d6798f82
zhangpaopao <zhangpaopao0609@gmail.com>
2024-01-11 16:25:56 +0800

    feat: ts

---
+++ packages/components/jsx.d.ts
@@ -0,0 +1,26 @@
+// import { HTMLAttributes } from 'vue';
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicAttributes {
+//       [emit: string]: any;
+//     }
+//   }
+// }
+
+// declare module '@vue/runtime-dom' {
+//   interface HTMLAttributes {
+//     [attr: string]: any;
+//   }
+
+//   interface CSSProperties {
+//     [attr: string]: any;
+//   }
+// }
+
+// declare module '@vue/runtime-core' {
+//   interface ComponentCustomProperties {
+//     // todo
+//     [x: string]: any;
+//   }
+// }
