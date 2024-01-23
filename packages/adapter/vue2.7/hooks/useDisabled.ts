f9b11698a110626f1953f68cf92351439d46ee56
zhangpaopao <zhangpaopao0609@gmail.com>
2024-01-23 12:57:53 +0800

    feat: switch

--- packages/adapter/vue2.7/hooks/useDisabled.ts
+++ packages/adapter/vue2.7/hooks/useDisabled.ts
@@ -18,16 +18,16 @@ export interface DisabledContext {
  */
 export function useDisabled(context?: DisabledContext) {
   const currentInstance = getCurrentInstance();
-  const componentDisabled = computed(() => currentInstance.props.disabled as boolean);
+  const componentDisabled = computed(() => currentInstance?.props?.disabled as boolean);
 
   const formDisabled = inject<FormDisabledProvider>('formDisabled', Object.create(null));
 
   return computed(() => {
-    if (isBoolean(context?.beforeDisabled.value)) return context.beforeDisabled.value;
+    if (isBoolean(context?.beforeDisabled?.value)) return context.beforeDisabled.value;
     // Component
     if (isBoolean(componentDisabled.value)) return componentDisabled.value;
     // ComponentGroup
-    if (isBoolean(context?.afterDisabled.value)) return context.afterDisabled.value;
+    if (isBoolean(context?.afterDisabled?.value)) return context.afterDisabled.value;
     // Form
     if (isBoolean(formDisabled.disabled?.value)) return formDisabled.disabled.value;
 
