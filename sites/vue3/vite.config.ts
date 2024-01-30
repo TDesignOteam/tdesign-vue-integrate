35f4c54e3123a43536cc346d122987dae72eafdf
zhangpaopao <zhangpaopao0609@gmail.com>
2024-01-30 16:01:10 +0800

    feat: vscode

--- sites/vue3/vite.config.ts
+++ sites/vue3/vite.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite'
+import * as path from 'path';
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import * as path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 
 import TDocPlugin from './plugins/doc';
 import PWA from "./configs/pwa";
-
-import { searchForWorkspaceRoot } from 'vite'
-
-
 
 const workspaceRoot = searchForWorkspaceRoot(process.cwd())
 const getRootPath = (...args: string[]) => path.posix.resolve(workspaceRoot, ...args)
 
 
-const resolveConfig = (vueVersion: number) => {
+const resolveAlias = (vueVersion: number) => {
   return {
     '@adapter/vue': getRootPath(`packages/adapter/vue/vue${vueVersion}`),
     '@adapter/hooks': getRootPath(`packages/adapter/hooks/vue${vueVersion}`),
@@ -24,77 +20,45 @@ const resolveConfig = (vueVersion: number) => {
   }
 }
 
-
 const publicPathMap = {
   preview: '/',
   intranet: '/vue-next/',
   production: 'https://static.tdesign.tencent.com/vue-next/',
 };
 
-
 // ! vue-next 中是从 ../script/vite.base.config 引入
 const isCustomElement = (tag) => tag.startsWith('td-') || tag.startsWith('tdesign-theme');
 
-// ! vue-next 中是从 ../script/vite.base.config 引入
-export const basePlugin = [
-  [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement,
-        },
-      },
-    }),
-    vueJsx({
-      isCustomElement,
-    }),
-  ],
-];
-
-export const transformIntel = () => ({
-  name: 'transform-intel',
-  transform(code, id) {
-    const intelReg = /from "TDesign\/intel/g;
-    code = code.replace(intelReg, 'from "./vue3');
-    
-    return code;
-  }
-})
-
-const CONFIG = {
-  vue3: {
-    port: 17000,
-    plugins: [
-      ...basePlugin
-    ]
-  },
-}
-
 export default defineConfig(({ mode }) => {
-  const vueVersion = mode === 'vue2' ? 2 : 3;
-  
   return {
     base: publicPathMap[mode],
     resolve: {
       alias: {
-        ...resolveConfig(vueVersion),
         '@': path.resolve(__dirname),
+        ...resolveAlias(3),
       }
     },
-    
     server: {
       host: '0.0.0.0',
-      port: CONFIG[mode].port,
+      port: 1700,
       open: '/',
       fs: {
         allow: ['..'],
       },
     },
     plugins: [
-      transformIntel(),
       VitePWA(PWA),
       TDocPlugin(),
-      ...CONFIG[mode].plugins
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement,
+          },
+        },
+      }),
+      vueJsx({
+        isCustomElement,
+      }),
     ],
     optimizeDeps: {
       include: ['prismjs', 'prismjs/components/prism-bash.js', 'tdesign-icons-vue-next'],
