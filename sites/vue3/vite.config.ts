59041d47093ede2cec2a72dccadb8e919f18cbc4
zhangpaopao <zhangpaopao0609@gmail.com>
2024-01-31 14:58:38 +0800

    feat: port

--- sites/vue3/vite.config.ts
+++ sites/vue3/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       host: '0.0.0.0',
-      port: 1700,
+      port: 17000,
       open: '/',
       fs: {
         allow: ['..'],
