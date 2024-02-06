a77fd46b8a90c4fb0464a6f6561181665b44daaa
Uyarn <uyarnchen@gmail.com>
2024-02-06 16:33:50 +0800

    feat: button

---
+++ packages/intel/vue3/components/button/_example-ts/base.vue
@@ -0,0 +1,8 @@
+<template>
+  <t-space size="24px">
+    <t-button theme="default" variant="base">TS填充按钮</t-button>
+    <t-button theme="default" variant="outline">TS描边按钮</t-button>
+    <t-button theme="default" variant="dashed">TS虚框按钮</t-button>
+    <t-button theme="default" variant="text">TS文字按钮</t-button>
+  </t-space>
+</template>
