4e2256a5451d50d23d9f9a188ed773f7f5ae7bdf
zhangpaopao <zhangpaopao0609@gmail.com>
2024-04-01 19:15:44 +0800

    feat: dropdown

--- packages/adapter/utils/common/src/helper.ts
+++ packages/adapter/utils/common/src/helper.ts
@@ -1,11 +1,8 @@
 import camelCase from 'lodash/camelCase';
-import { isUndefined } from 'lodash-es';
+import { isArray, isNumber, isString, isUndefined } from 'lodash-es';
 import isNull from 'lodash/isNull';
-import { isArray } from 'lodash-es';
-import { isNumber } from 'lodash-es';
-import { isString } from 'lodash-es';
 
-// ! 是不是直接用 lodash 就成了？
+// vue23:! 是不是直接用 lodash 就成了？
 export function omit(obj: object, fields: string[]): object {
   const shallowCopy: Record<string, any> = {
     ...obj,
@@ -66,14 +63,16 @@ export function getCharacterLength(str: string, maxCharacter?: number) {
 export function getIEVersion() {
   const { userAgent } = navigator;
   // 判断是否IE<11浏览器
-  const isIE = userAgent.indexOf('compatible') > -1 && userAgent.indexOf('MSIE') > -1;
+  const isIE = userAgent.includes('compatible') && userAgent.includes('MSIE');
   // 判断是否IE11浏览器
-  const isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf('rv:11.0') > -1;
+  const isIE11 = userAgent.includes('Trident') && userAgent.includes('rv:11.0');
   if (isIE) {
     const reIE = new RegExp('MSIE (\\d+\\.\\d+);');
     const match = userAgent.match(reIE);
-    if (!match) return -1;
-    const fIEVersion = parseFloat(match[1]);
+    if (!match) {
+      return -1;
+    }
+    const fIEVersion = Number.parseFloat(match[1]);
     return fIEVersion < 7 ? 6 : fIEVersion;
   }
   if (isIE11) {
@@ -82,4 +81,13 @@ export function getIEVersion() {
   }
   // 不是ie浏览器
   return Number.MAX_SAFE_INTEGER;
+}
+
+/**
+ * 兼容样式中支持number/string类型的传值 得出最后的结果。
+ * @param param number或string类型的可用于样式上的值
+ * @returns 可使用的样式值。
+ */
+export function pxCompat(param: string | number) {
+  return isNumber(param) ? `${param}px` : param;
 }
