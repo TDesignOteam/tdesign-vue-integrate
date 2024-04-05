df36ef9af0381a64dc63b1e9beab14e9cecd3fb7
Uyarn <uyarnchen@gmail.com>
2024-04-06 01:26:07 +0800

    chore: fix demo

--- packages/intel/vue2.7/components/popup/_example/plugin.vue
+++ packages/intel/vue2.7/components/popup/_example/plugin.vue
@@ -1,20 +1,5 @@
-<template>
-  <t-space direction="vertical">
-    <t-button variant="outline" @click="handleElement1" class="trigger-element1">正常的方式打开</t-button>
-    <t-button variant="outline" @click="handleElement2" class="trigger-element2"
-    >通过Plugin打开，并修改不同浮层的配置</t-button
-    >
-    <div>
-      <span>这里是一个日志查询的例子，在很长的日志内容中，日志内容存在换行的情况，可以点击链接进行日志查询操作</span>
-      <a class="trigger-element3" style="color: var(--td-text-color-brand)" @click="handleCreatePopupOffset"
-      >点击此链接，会打开浮层进行跳转操作</a
-      >
-    </div>
-  </t-space>
-</template>
-
 <script lang="jsx">
-// import { PopupPlugin } from 'tdesign-vue';
+import { PopupPlugin } from 'tdesign-vue';
 
 export default {
   methods: {
@@ -29,7 +14,7 @@ export default {
     },
     handleCreatePopupOffset() {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      this.$popup('.trigger-element3', (h) => <div>透传popperOptions，在offset里控制节点位置</div>, {
+      this.$popup('.trigger-element3', h => <div>透传popperOptions，在offset里控制节点位置</div>, {
         placement: 'bottom',
         popperOptions: {
           modifiers: [
@@ -47,7 +32,7 @@ export default {
                     lineHeight = `${temp.offsetHeight}px`;
                     document.body.removeChild(temp);
                   }
-                  const isBreakLine = reference.height > parseInt(lineHeight, 10);
+                  const isBreakLine = reference.height > Number.parseInt(lineHeight, 10);
                   return isBreakLine ? [reference.x, -reference.height + 10] : [0, 0];
                 },
               },
@@ -59,6 +44,22 @@ export default {
   },
 };
 </script>
+
+<template>
+  <t-space direction="vertical">
+    <t-button variant="outline" class="trigger-element1" @click="handleElement1">
+      正常的方式打开
+    </t-button>
+    <t-button variant="outline" class="trigger-element2" @click="handleElement2">
+      通过Plugin打开，并修改不同浮层的配置
+    </t-button>
+    <div>
+      <span>这里是一个日志查询的例子，在很长的日志内容中，日志内容存在换行的情况，可以点击链接进行日志查询操作</span>
+      <a class="trigger-element3" style="color: var(--td-text-color-brand)" @click="handleCreatePopupOffset">点击此链接，会打开浮层进行跳转操作</a>
+    </div>
+  </t-space>
+</template>
+
 <style scoped>
 .trigger-element3 {
   text-decoration: underline;
